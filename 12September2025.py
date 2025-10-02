2c2d46dcdf37dbc72683a3138912c33eb4085bac
Preet Chauhan <preet.24b0101197@abes.ac.in>
2025-10-02 23:15:30 +0530

    Add files via upload

---
+++ 12September2025.py
@@ -0,0 +1,60 @@
+# x=int(input(" Enter Your Marks: "))
+
+# if(x>95):
+    
+#     print("grade: Excellent")
+    
+# elif(x>80 and x<=90):
+#      print("grade: Good")
+# elif(x>60 and x<=80):
+#      print("grade: Average")
+# else:
+#      print("grade:Below average")
+    
+    # Check Temperature
+# unit=input("ENter the unit: C or F:")
+# temp=int(input("Enter the Temperature:"))
+
+
+# if(unit=="C"):
+#     temp=(((9*temp)/5)+32)
+#     print(f" The temperature is {temp} °F")
+# else:
+#      temp=((temp-32)*5)/9
+#      print(f" The temperature is {temp} °C")
+    
+
+    
+# y=3*0.1
+# if y!=0.3:
+#     print("Launch a Missile")
+# else:
+#     print("Let us have Peace")
+
+# x=3
+# if(x>2):
+#     x=x*2
+# if(x>4):
+#     x=0
+#     print(x)
+#     sum=0
+#     for i in range(1,20):
+#         sum+=i
+#         print(sum)
+# print(f"The sum is {sum}")
+
+
+# for i in (1,2,3,4):
+#     print(i)
+# for i in "Hello":
+#      print(i)
+     
+# for i in [1,2,3,4]:
+#      print(i)
+
+# for i in {1:'a',2:"b",3:'a',4:"c"}:
+#      print(i)
+
+# List=[]
+# tuple=()
+# dictionary={}
