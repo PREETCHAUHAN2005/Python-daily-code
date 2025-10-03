ef5fb199b636c500791ef460996cdb18b3a81c85
Preet Chauhan <preet.24b0101197@abes.ac.in>
2025-10-03 23:32:02 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+l1=["Apple","Name","12.5","13","None","True","2+3j"]
+# print(l1[:])
+# print(l1[::])
+# print(l1[::-1])
+# print(l1[::2])
+# print(l1[1:3:1])
+# print(l1[1:5:2])
+
+# l2=list(input("Ente the list elements separated by comma: ").split(" "))
+# del l2[2]
+# print(l2)
+
+x=['xy','yz']
+for i in range(len(x)):
+    x[i]=x[i].upper()
+    # x[1]=i.upper()
+print(x)
